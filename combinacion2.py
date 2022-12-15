b524d15ee317bfecffb6f5831c5ae66952e4ec91
Paloma Núñez Reyes <palomanunezreyes@gmail.com>
2022-12-15 09:59:37 +0100

    pruebas

---
+++ combinacion2.py
@@ -0,0 +1,25 @@
+# Example of input -> {"3XX": 11, "1CX": 12, "6XX": 15, "5XX": 16} -> Position 0 is front of the line
+# Function returns array with all possinle combinatins
+
+def array_combination():
+    # importing Numpy package
+    import numpy as np
+
+    # creating 2 numpy arrays
+    array_1 = np.array([1])
+    array_2 = np.array([2, 4, 6])
+
+    print("Array-1")
+    print(array_1)
+
+    print("\nArray-2")
+    print(array_2)
+
+    # combination of elements of array_1 and array_2
+    # using numpy.meshgrid().T.reshape()
+    comb_array = np.array(np.meshgrid(array_1, array_2)).T.reshape(-1, 2)
+
+    print("\nCombine array:")
+    print(comb_array)
+
+array_combination()
