7347172e35ba0095d8000ba6e49883f315672f15
Paloma Núñez Reyes <palomanunezreyes@gmail.com>
2022-12-15 21:18:16 +0100

    pruebas

--- parte-2/ASTARColaBus.py
+++ parte-2/ASTARColaBus.py
@@ -73,27 +73,27 @@ class Estado:
         return self.f >= other.f
 
     def calcular_g(self) -> int:
-        """ PARA CONSEGUIR EL COSTE DE MOVER DE UN ESTADO A OTRO """
-        alumnos_conflictivos = []
+        """ PARA CONSEGUIR EL COSTE DE MOVER DE UN ESTADO A OTRO
+        los alumnols conflictivos duplican el tiempo del alumno actual si se sienta atras.
+        conflictivo, anadirlo a los alumnos_conflictivos.
+        anterior es conflictivo duplica el coste.
+        siguiente es conflictivo, duplica el coste.
+        si el actual es de movilidad reducida, triplicar el coste.
+        ayuda a un alumno con movilidad reducida, los cost_list se multiplican y se anula el que tiene el ayudante."""
         cost_list = [1] * len(self.cola)
+        alumnos_conflictivos = []
         for i in range(len(self.cola)):
-            # los alumnols conflictivos duplican el tiempo del alumno actual si se sienta atras.
             for j in alumnos_conflictivos:
                 if self.cola[i].asiento > j.asiento:
                     cost_list[i] *= 2
-            # Si es conflictivo, anadirlo a los alumnos_conflictivos.
             if self.cola[i].conflictivo:
                 alumnos_conflictivos.append(self.cola[i])
-            # Si el anterior es conflictivo, duplicar el coste.
             if i > 0 and self.cola[i - 1].conflictivo:
                 cost_list[i] *= 2
-            # Si el siguiente es conflictivo, duplicar el coste.
             if i + 1 < len(self.cola) and self.cola[i + 1].conflictivo:
                 cost_list[i] *= 2
-            # Si el actual es de movilidad reducida, triplicar el coste.
             if self.cola[i].movilidad_reducida:
                 cost_list[i] *= 3
-        # Si se ayuda a un alumno con movilidad reducida, los cost_list se multiplican y se anula el que tiene el ayudante.
         for i in range(len(self.cola)):
             if self.cola[i].movilidad_reducida and i + 1 < len(self.cola):
                 cost_list[i] *= cost_list[i + 1]
@@ -105,7 +105,7 @@ class Estado:
         return total_cost
 
     def calcular_h(self, heuristica: int) -> int:
-        """ Metodo que devuelve el valor heuristico del estado. """
+        """  devuelve el valor heuristico del estado. """
         if heuristica == 1:
             return self.calcular_h_1()
         if heuristica == 2:
@@ -117,58 +117,57 @@ class Estado:
         return 0
 
     def calcular_h_1(self) -> int:
-        """ Metodo que devuelve el valor heuristico del estado usando la heuristica 1. """
+        """  usando la heuristica 1. """
         conflictivos = 0
         movilidad_reducida = 0
-        for e in self.cola:
-            if e.conflictivo:
+        for i in self.cola:
+            if i.conflictivo:
                 conflictivos += 1
-            if e.movilidad_reducida:
+            if i.movilidad_reducida:
                 movilidad_reducida += 1
         return len(self.cola) + conflictivos * 3 + movilidad_reducida * 2
 
     def calcular_h_2(self) -> int:
-        """ Metodo que devuelve el valor heuristico del estado usando la heuristica 2. """
+        """ usando la heuristica 2. """
         conflictivos = 0
         movilidad_reducida = 0
-        for e in self.cola:
-            if e.conflictivo:
+        for i in self.cola:
+            if i.conflictivo:
                 conflictivos += 1
-            if e.movilidad_reducida:
+            if i.movilidad_reducida:
                 movilidad_reducida += 1
         return len(self.cola) + conflictivos * 3 + movilidad_reducida
 
     def calcular_h_3(self) -> int:
-        """ Metodo que devuelve el valor heuristico del estado usando la heuristica 3. """
+        """  usando la heuristica 3. """
         conflictivos = 0
         movilidad_reducida = 0
-        for e in self.cola:
-            if e.conflictivo:
+        for i in self.cola:
+            if i.conflictivo:
                 conflictivos += 1
-            if e.movilidad_reducida:
+            if i.movilidad_reducida:
                 movilidad_reducida += 1
         return len(self.cola) + conflictivos + movilidad_reducida * 2
 
     def calcular_h_4(self) -> int:
-        """ Metodo que devuelve el valor heuristico del estado usando la heuristica 4. """
+        """  usando la heuristica 4. """
         conflictivos = 0
         movilidad_reducida = 0
-        for e in self.cola:
-            if e.conflictivo:
+        for i in self.cola:
+            if i.conflictivo:
                 conflictivos += 1
-            if e.movilidad_reducida:
+            if i.movilidad_reducida:
                 movilidad_reducida += 1
         return len(self.cola) + conflictivos + movilidad_reducida
 
-    def es_meta(self) -> bool:
-        """ Metodo que indica si el estado es un estado meta. """
+    def llegamos_goal(self) -> bool:
         return len(self.por_asignar) == 0
 
     def expandir(self, heuristica: int) -> list:
-        """ Metodo que devuelve una lista con los estados obtenidos tras expandir el actual. """
+        """  lista con los estados despues de expandir """
         expandir = []
         for alumno in self.por_asignar:
-            if self.futuro_valido(alumno):
+            if self.estado_siguiente_adecuado(alumno):
                 cola = self.cola.copy()
                 cola.append(alumno)
                 sin_asignar = self.por_asignar.copy()
@@ -178,65 +177,62 @@ class Estado:
         return expandir
 
     def de_dict(self, dict_entrada: dict) -> None:
-        """ Metodo que anade a la cola los alumnos de un diccionario. """
-        for clave, valor in dict_entrada.items():
-            self.por_asignar.append(Alumno(clave, valor))
+        """ incluimos a la cola los alumnos de un diccionario. """
+        for id, caracteristica in dict_entrada.items():
+            self.por_asignar.append(Alumno(id, caracteristica))
 
     def a_dict(self) -> dict:
-        """ Metodo que devuelve la cola de un estado como un diccionario. """
+        """ cola de un estado como un diccionario """
         a_dict = {}
         for i in self.cola:
             a_dict[i.clave] = i.asiento
         return a_dict
 
-    def futuro_valido(self, alumno: Alumno) -> bool:
-        """ Metodo que indica si un futuro estado sera valido. """
-        """ Existen ayudantes para todos los alumnos con movilidad reducida y 
-        el actual no tiene movilidad reducida o es el primero en la cola o el anterior no tiene movilidad reducida. """
-        return self.quedan_ayudantes(alumno) \
+    def estado_siguiente_adecuado(self, alumno: Alumno) -> bool:
+        """ hay ayudantes para los alumnos """
+        return self.alumnos_que_ayudan(alumno) \
             and (not alumno.movilidad_reducida or len(self.cola) == 0 or not self.cola[-1].movilidad_reducida)
 
-    def quedan_ayudantes(self, alumno: Alumno) -> bool:
-        """ Metodo que indica si en un futuro estado habra ayudantes suficientes para los alumnos con movilidad
-        reducida. """
+    def alumnos_que_ayudan(self, alumno: Alumno) -> bool:
+        """ ver si en el siguiente estado habrán suficientes alumnos para ayudar a los de mov. reducida"""
         movilidad_reducida = 0
         for i in self.por_asignar:
             if i.movilidad_reducida:
                 movilidad_reducida += 1
-        movilidad_normal = len(self.por_asignar) - movilidad_reducida
+        alumno_sin_reducida = len(self.por_asignar) - movilidad_reducida
         if not alumno.movilidad_reducida:
-            movilidad_normal -= 1
-        return movilidad_reducida <= movilidad_normal
+            alumno_sin_reducida -= 1
+        return movilidad_reducida <= alumno_sin_reducida
 
 
 class AEstrella:
     def __init__(self, dict_alumnos: dict, heuristica: int):
         estado = Estado(heuristica=heuristica)
         estado.de_dict(dict_alumnos)
-        self.abierta = [estado]
-        self.expandidos = 0
         self.heuristica = heuristica
         self.meta = Estado()
-        self.tiempo = 0
+        self.tiempo, self.expandidos = 0
+        self.abierta = [estado]
+
 
     def ejecutar(self) -> None:
-        """ Metodo que ejecuta el algoritmo A*. """
-        principio = datetime.datetime.now()
+        """ algoritmo A*. """
+        inicio = datetime.datetime.now()
         while len(self.abierta) > 0:
             nodo = self.abierta.pop(0)
-            if nodo.es_meta():
+            if nodo.llegamos_goal():
                 self.meta = nodo
-                diferencia_tiempo = datetime.datetime.now() - principio
+                diferencia_tiempo = datetime.datetime.now() - inicio
                 self.tiempo = diferencia_tiempo.total_seconds()
                 return
             self.abierta += nodo.expandir(self.heuristica)
             self.expandidos += 1
             self.abierta.sort()
-        raise Exception('Es imposible obtener una solucion que satisfaga las restricciones.')
+        raise Exception('imposible conseguir una solucion que satisfaga las restricciones.')
 
     @property
     def stat(self) -> str:
-        """ Propiedad que devuelve el contenido del fichero stat. """
+        """  devuelve el contenido del fichero de estadisticas """
         return 'Tiempo total: ' + str(self.tiempo) \
             + '\nCoste total: ' + str(self.meta.g) \
             + '\nLongitud del plan: ' + str(self.meta.profundidad) \
@@ -244,23 +240,22 @@ class AEstrella:
 
 
 def leer_entrada(ruta: str) -> dict:
-    """ Funcion que lee un fichero de entrada y lo devuelve como un diccionario. """
+    """ fichero entrada devuelto como diccionario """
     with open(ruta) as fichero:
-        contenido = fichero.read()
-    entrada = eval(contenido)
+        fichero_interior = fichero.read()
+    entrada = eval(fichero_interior)
     if not isinstance(entrada, dict):
-        raise Exception('Fichero de entrada invalido.')
+        raise Exception('Fichero de entrada no es correcto.')
     return entrada
 
 
 def escribir_salida(ruta: str, aestrella_ejecutado: AEstrella) -> None:
-    """ Funcion que escribe dos ficheros de salida con los datos de la ejecucion. """
     escribir_salida_output(ruta, aestrella_ejecutado)
     escribir_salida_stat(ruta, aestrella_ejecutado)
 
 
 def escribir_salida_output(ruta: str, aestrella_ejecutado: AEstrella) -> None:
-    """ Funcion que escribe el fichero de salida .output con las configuraciones de la cola al inicio y al final. """
+    """  fichero de salida  configuraciones de la cola inicio y  final. """
     output = "INICIAL:\t"
     with open(ruta) as fichero:
         output += fichero.read()
@@ -271,18 +266,15 @@ def escribir_salida_output(ruta: str, aestrella_ejecutado: AEstrella) -> None:
 
 
 def escribir_salida_stat(ruta: str, aestrella_ejecutado: AEstrella) -> None:
-    """ Funcion que escribe el fichero de salida .stat con la informacion del proceso de busqueda. """
+    """  fichero de salida estadisticas """
     ruta_salida = ruta[:-5] + '-' + str(aestrella_ejecutado.heuristica) + '.stat'
     with open(ruta_salida, 'w') as fichero:
         fichero.write(aestrella_ejecutado.stat)
 
 
 if __name__ == '__main__':
-    # Crear el Objeto con el diccionario del fichero de entrada y la heuristica deseada
     aestrella = AEstrella(leer_entrada(sys.argv[1]), int(sys.argv[2]))
-    # Ejecutar el algoritmo A*
     aestrella.ejecutar()
-    # Escribir los ficheros de salida
     escribir_salida(sys.argv[1], aestrella)
     print(aestrella.meta.a_dict())
 
