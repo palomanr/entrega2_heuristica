4c12c816f702772a7cf8810394d79c6a4c8a0c0c
ImproveEu <95770963+ImproveEu@users.noreply.github.com>
2022-12-15 08:14:41 +0100

    mejora de madrugada

---
+++ parte-1/CSP-calls.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo ------------PRUEBA 1--------------------
+python3 Bus_CSP.py ./CSP-tests/alumnos1.txt
+echo ------------PRUEBA 2--------------------
+python3 Bus_CSP.py ./CSP-tests/alumnos2.txt
+echo ------------PRUEBA 3--------------------
+python3 Bus_CSP.py ./CSP-tests/alumnos3.txt
+echo ------------PRUEBA 4--------------------
+python3 Bus_CSP.py ./CSP-tests/alumnos4.txt
+echo ------------PRUEBA 5--------------------
+python3 Bus_CSP.py ./CSP-tests/alumnos5.txt
