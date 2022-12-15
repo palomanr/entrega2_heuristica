65f1e4f439e3d812fbc3d119d7eaea2c911447e5
Paloma Núñez Reyes <palomanunezreyes@gmail.com>
2022-12-15 17:35:22 +0100

    pruebas

--- parte-2/ASTAR-calls.sh
+++ parte-2/ASTAR-calls.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
-echo ------------PRUEBA 1--------------------
-python3 ASTARColaBus.py ./ASTAR-tests/alumnos1
-echo ------------PRUEBA 2--------------------
-python3 ASTARColaBus.py ./ASTAR-tests/alumnos2
-echo ------------PRUEBA 3--------------------
-python3 ASTARColaBus.py ./ASTAR-tests/alumnos3
-echo ------------PRUEBA 4--------------------
-python3 ASTARColaBus.py ./ASTAR-tests/alumnos4
-echo ------------PRUEBA 5--------------------
-python3 ASTARColaBus.py ./ASTAR-tests/alumnos5
+echo ------------PRUEBA 1 HEURISTICA 1--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos1.prob 1
+echo ------------PRUEBA 1 HEURISTICA 2--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos1.prob 2
+echo ------------PRUEBA 1 HEURISTICA 3--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos1.prob 3
+echo ------------PRUEBA 1 HEURISTICA 4--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos1.prob 4
+echo ------------PRUEBA 2 HEURISTICA 1--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos2.prob 1
+echo ------------PRUEBA 2 HEURISTICA 2--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos2.prob 2
+echo ------------PRUEBA 2 HEURISTICA 3--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos2.prob 3
+echo ------------PRUEBA 2 HEURISTICA 4--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos2.prob 4
+echo ------------PRUEBA 3 HEURISTICA 1--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos3.prob 1
+echo ------------PRUEBA 3 HEURISTICA 2--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos3.prob 2
+echo ------------PRUEBA 3 HEURISTICA 3--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos3.prob 3
+echo ------------PRUEBA 3 HEURISTICA 4--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos3.prob 4
+echo ------------PRUEBA 4 HEURISTICA 1--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos4.prob 1
+echo ------------PRUEBA 4 HEURISTICA 2--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos4.prob 2
+echo ------------PRUEBA 4 HEURISTICA 3--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos4.prob 3
+echo ------------PRUEBA 4 HEURISTICA 4--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos4.prob 4
+echo ------------PRUEBA 5 HEURISTICA 1--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos5.prob 1
+echo ------------PRUEBA 5 HEURISTICA 2--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos5.prob 2
+echo ------------PRUEBA 5 HEURISTICA 3--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos5.prob 3
+echo ------------PRUEBA 5 HEURISTICA 4--------------------
+python ASTARColaBus.py ./ASTAR-tests/alumnos5.prob 4
